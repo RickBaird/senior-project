b44a5fcea1c6b4606c56f0e1780006543c0973ce
Karl-Cebulski <56772650+Karl-Cebulski@users.noreply.github.com>
2020-03-07 15:29:14 -0500

    Update user.dart

--- lib/user.dart
+++ lib/user.dart
@@ -5,6 +5,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 import 'package:ruroomates/Chat.dart';
 import 'package:ruroomates/const.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'const.dart';
 
@@ -24,58 +25,89 @@ class UserProfile extends StatelessWidget{
 class OtherUserProfile extends StatefulWidget{
   OtherUserProfile({Key key, this.title}) : super(key: key);
   final String title;
+  String peerID;
+  String peerPic;
 
   @override
-  _OtherUserProfileState createState() => _OtherUserProfileState();
+  _OtherUserProfileState createState() => _OtherUserProfileState(peerID: peerID, peerPic: peerPic);
 }
 
 
 class _OtherUserProfileState extends State<OtherUserProfile> {
+_OtherUserProfileState({Key key, @required this.peerID, @required this.peerPic});
 
   DocumentSnapshot document;
+  SharedPreferences prefs;
+  String profileViewID;
+  String peerID;
+  String peerPic;
+  String id;
+
+readLocal() async {
+  prefs = await SharedPreferences.getInstance();
+  id = prefs.getString('id') ?? '';
+  if (id.hashCode <= peerID.hashCode) {
+    profileViewID = '$id-$peerID';
+  } else {
+    profileViewID= '$peerID-$id';
+  }
+
+  Firestore.instance.collection('users').document(id).updateData({'Viewing': peerID});
+
+  setState(() {});
+}
 
   @override
   Widget build(BuildContext context) {
-    Column(
-      children: <Widget>[
-        Row(
-          children: <Widget>[
-            Material(
-              child: document['photoUrl'] != null ? CachedNetworkImage(
-                placeholder: (context, url) => Container(
-                  child: CircularProgressIndicator(
-                    strokeWidth: 1.0,
-                    valueColor: AlwaysStoppedAnimation<Color>(themeColor),
-                  ),
+  DocumentSnapshot document;
+  if (document['peerID'] == id) {
+    return
+      Column(
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              Material(
+                child: document['photoUrl'] != null ? CachedNetworkImage(
+                  placeholder: (context, url) =>
+                      Container(
+                        child: CircularProgressIndicator(
+                          strokeWidth: 1.0,
+                          valueColor: AlwaysStoppedAnimation<Color>(themeColor),
+                        ),
+                        width: 50.0,
+                        height: 50.0,
+                        padding: EdgeInsets.all(15.0),
+                      ),
+                  imageUrl: document['photoUrl'],
                   width: 50.0,
                   height: 50.0,
-                  padding: EdgeInsets.all(15.0),
+                  fit: BoxFit.cover,
+                )
+                    : Icon(
+                  Icons.account_circle,
+                  size: 50.0,
+                  color: greyColor,
                 ),
-                imageUrl: document['photoUrl'],
-                width: 50.0,
-                height: 50.0,
-                fit: BoxFit.cover,
+                borderRadius: BorderRadius.all(Radius.circular(25.0)),
+                clipBehavior: Clip.hardEdge,
+              ),
+              Row(
+                  children: <Widget>[
+                    Flexible(
+                      child: Container(
+                        child: Text(
+                          'Name: ${document['nickname']}',
+                          style: TextStyle(color: primaryColor),
+                        ),
+                      ),
+                    ),
+                  ]
               )
-                  : Icon(
-                Icons.account_circle,
-                size: 50.0,
-                color: greyColor,
-              ),
-              borderRadius: BorderRadius.all(Radius.circular(25.0)),
-              clipBehavior: Clip.hardEdge,
-            ),
-            Flexible(
-              child: Container(
-                child: Text(
-                  'Name: ${document['nickname']}',
-                  style: TextStyle(color: primaryColor),
-                ),
-              ),
-            )
-          ],
-        )
-      ],
-    );
+            ],
+          )
+        ],
+      );
+  }
   }
 
 }
